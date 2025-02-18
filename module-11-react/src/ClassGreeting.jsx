022e680325bc9a568aa8ec07c987a2a4bd8f5f63
Lauren Farrell <150389282+BUFFALOLO@users.noreply.github.com>
2025-02-17 19:20:08 -0500

    Delete module-11-react directory

--- module-11-react/src/ClassGreeting.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-
-class ClassGreeting extends Component {
-    render() {
-        return <p>This is a class-based greeting!</p>;
-    }
-}
-export default ClassGreeting;
--- module-11-react/src/Greeting.jsx
+++
@@ -1,9 +0,0 @@
-// src/Greeting.jsx
-
-import React from 'react';
-
-function Greeting() {
-    return <p>Welcome to the world of React!</p>;
-}
-
-    export default Greeting;
